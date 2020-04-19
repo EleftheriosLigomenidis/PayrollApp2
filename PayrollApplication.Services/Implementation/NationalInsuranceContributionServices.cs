a9ca025d22f1ad7398bbf4605c753c98d2ad8aaf
lef <ligolef@gmail.com>
2020-04-19 11:56:02 +0300

    NationalInsurance Interface and implementation

---
+++ PayrollApplication.Services/Implementation/NationalInsuranceContributionServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollApplication.Services.Implementation
+{
+    public class NationalInsuranceContributionServices :INationalInsuranceContributionServices
+    {
+        private decimal NIRate;
+        private decimal NIC;
+
+        public decimal NIContribution(decimal totalAmount)
+        {
+            if(totalAmount < 719)
+            {
+                NIRate = .0M;
+                NIC = 0M;
+            }
+            else if(totalAmount >719 && totalAmount <= 4167)
+            {
+                NIRate = .12M;
+                NIC = ((totalAmount - 719) * NIRate);
+            }
+            else if(totalAmount > 4167)
+            {
+                NIRate = .02M;
+                NIC = ((4167 - 719) * .12M) + ((totalAmount - 4167) * NIRate);
+            }
+
+            return NIC;
+        }
+    }
+}
--- PayrollApplication.Services/Implementation/TaxService.cs
+++ PayrollApplication.Services/Implementation/TaxService.cs
@@ -10,6 +10,7 @@ namespace PayrollApplication.Services.Implementation
         private decimal tax;
         public decimal TaxAmount(decimal totalAmount)
         {
+            //based on Uk
             if(totalAmount <= 1042)
             {
                 taxRate = .0m;
@@ -26,11 +27,14 @@ namespace PayrollApplication.Services.Implementation
             {
                 // higher tax rate
                 taxRate = .40m;
+                //income tax rate
                 tax = (1042 * .0m) + ((3125 - 1042) * .20m) + ((totalAmount - 3125) * taxRate);
             }
             else if(totalAmount > 12500)
             {
+                // highest tax rate
                 taxRate = .45m;
+                //income tax rate
                 tax = (1042 * .0m) + ((3125 - 1042) * .20m) + ((12500 - 3125) * .40m) + ((totalAmount - 12500) * taxRate);
             }
 
